c9b9b86494c9a5c4f63bf1755ceec333b85f8568
César <83521963+CesarBocel222@users.noreply.github.com>
2023-05-02 23:38:44 -0600

    Datos pacientes

---
+++ Pacientes.cs
@@ -0,0 +1,36 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+
+namespace Proyecto_ED1.Models.clss
+{
+    public class Pacientes
+    {
+        [Name("Nombre")]
+        public String Name { get; set; } 
+
+        [Name("ID")]
+        public String ID { get; set; }
+
+        [Name("Edad")]
+        public String age { get; set; }
+        [Name("Ultima consulta")]
+        public int last_consult { get; set; }
+        [Name("Proxima consulta")]
+        public int next_consult { get; set; }
+        [Name("Descripcion")]
+        public String description { get; set; }
+        
+
+        public Pacientes() { }
+        public Pacientes(String name, String id, String age, int last, int next, String description)
+        {
+            this.Name = name;
+            this.ID = id;
+            this.age = age;
+            this.last_consult = last;
+            this.next_consult = next;
+            this.description = description;
+            
+        }
+    }
+}
