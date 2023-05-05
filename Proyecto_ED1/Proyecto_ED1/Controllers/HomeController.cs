316be034cf4018d8b4cd131151fc7652b2000566
Pierriny <70544564+Pierriny@users.noreply.github.com>
2023-05-04 21:48:12 -0600

    Model Pacientes terminado y 2 views creadas

--- Proyecto_ED1/Proyecto_ED1/Controllers/HomeController.cs
+++ Proyecto_ED1/Proyecto_ED1/Controllers/HomeController.cs
@@ -33,5 +33,14 @@ namespace Proyecto_ED1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult recibirManualAVL()
+        {
+            return View();
+        }
+
+
+
+
     }
 }
